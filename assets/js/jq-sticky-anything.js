58fb50f94126fbfa4ebfbe33d14f00a922b065df
Mark Senff <mark@senff.com>
2015-05-14 09:16:02 -0400

    Update jq-sticky-anything.js

--- assets/js/jq-sticky-anything.js
+++ assets/js/jq-sticky-anything.js
@@ -1,5 +1,5 @@
 /**
-* @preserve Sticky Anything 1.1.4 | @senff | GPL2 Licensed
+* @preserve Sticky Anything 1.3 | @senff | GPL2 Licensed
 */
 
 (function ($) {
@@ -12,69 +12,125 @@
       minscreenwidth: 0, 
       maxscreenwidth: 99999, 
       zindex: 1, 
-      debugmode: false
+      dynamicmode: false,
+      debugmode: false,
+      pushup: '',
+      adminbar: false
       }, options );
 
     var numElements = $(this).length;
+    var numPushElements = $(settings.pushup).length;
+
+
+    if (numPushElements < 1) {
+      // There are no elements on the page with the called selector for the Push-up Element.
+      if(settings.debugmode == true) {
+        console.error('STICKY ANYTHING DEBUG: There are no elements with the selector/class/ID you selected for the Push-up element ("'+settings.pushup+'").');
+      }
+      // Resetting it to NOTHING.
+      settings.pushup = '';
+    } else if (numPushElements > 1) {
+      // You can't use more than one element to push up the sticky element.
+      // Make sure that you use a selector that applies to only ONE SINGLE element on the page.
+      // Want to find out quickly where all the elements are that you targeted? Uncomment the next line to debug.
+      // $(settings.pushup).css('border','solid 3px #ff0000');
+      if(settings.debugmode == true) {
+        console.error('STICKY ANYTHING DEBUG: There are '+numPushElements+' elements on the page with the selector/class/ID you selected for the push-up element ("'+settings.pushup+'"). You can select only ONE element to push the sticky element up.');
+      }  
+      // Resetting it to NOTHING.
+      settings.pushup = '';
+    } 
+
 
     if (numElements < 1) {
       // There are no elements on the page with the called selector.
       if(settings.debugmode == true) {
-        console.error('STICKY ANYTHING DEBUG: There are no elements with the selector/class/ID you selected.');
+        console.error('STICKY ANYTHING DEBUG: There are no elements with the selector/class/ID you selected for the sticky element ("'+this.selector+'").');
       }
     } else if (numElements > 1) {
       // This is not going to work either. You can't make more than one element sticky. They will only get in eachother's way.
-      // Make sure that you use an selector that applies to only ONE SINGLE element on the page.
+      // Make sure that you use a selector that applies to only ONE SINGLE element on the page.
       // Want to find out quickly where all the elements are that you targeted? Uncomment the next line to debug.
       // $(this).css('border','solid 3px #00ff00');
       if(settings.debugmode == true) {
-        console.error('STICKY ANYTHING DEBUG: There is more than one element with the selector/class/ID you selected. You can only make ONE element sticky.');
-      }      
+        console.error('STICKY ANYTHING DEBUG: There There are '+numPushElements+' elements with the selector/class/ID you selected for the sticky element ("'+this.selector+'"). You can only make ONE element sticky.');
+      }  
     } else {
-      // Create a clone of the menu, right next to original (in the DOM).
-      // OLD: $(this).addClass('original').clone().insertAfter(this).addClass('cloned').css('position','fixed').css('top',settings.top+'px').css('margin-top','0').css('margin-left','0').css('z-index',settings.zindex).removeClass('original').hide();
       $(this).addClass('original');
-      checkElement = setInterval(function(){stickIt(settings.top,settings.minscreenwidth,settings.maxscreenwidth,settings.zindex)},10);
+      if(settings.dynamicmode != true) {
+        // Create a clone of the menu, right next to original (in the DOM) on initial page load
+        createClone(settings.top,settings.zindex,settings.adminbar);
+      }
+
+      checkElement = setInterval(function(){stickIt(settings.top,settings.minscreenwidth,settings.maxscreenwidth,settings.zindex,settings.pushup,settings.dynamicmode,settings.adminbar)},10);
     }
 
     return this;
   };
 
 
-function stickIt(stickyTop,minwidth,maxwidth,stickyzindex) {
+  function stickIt(stickyTop,minwidth,maxwidth,stickyZindex,pushup,dynamic,adminbar) {
 
-  var orgElementPos = $('.original').offset();
-  orgElementTop = orgElementPos.top;               
+    var orgElementPos = $('.original').offset();
+    var pushElementPos = $(pushup).offset();
+    orgElementTop = orgElementPos.top;               
+    pushElementTop = pushElementPos.top;    
 
-  // Calculating actual viewport width
-  var e = window, a = 'inner';
-  if (!('innerWidth' in window )) {
-    a = 'client';
-    e = document.documentElement || document.body;
-  }
-  viewport = e[ a+'Width' ];
+    // Calculating actual viewport width
+    var e = window, a = 'inner';
+    if (!('innerWidth' in window )) {
+      a = 'client';
+      e = document.documentElement || document.body;
+    }
+    viewport = e[ a+'Width' ];
 
-  if (($(window).scrollTop() >= (orgElementTop - stickyTop)) && (viewport >= minwidth) && (viewport <= maxwidth)) {
-
-    // scrolled past the original position; now only show the cloned, sticky element.
-
-    // Cloned element should always have same left position and width as original element.     
-    orgElement = $('.original');
-    coordsOrgElement = orgElement.offset();
-    leftOrgElement = coordsOrgElement.left;  
-    widthOrgElement = orgElement.css('width');
-
-    if($('.cloned').length < 1) {
-      $('.original').clone().insertAfter('.original').addClass('cloned').css('position','fixed').css('top',stickyTop+'px').css('margin-top','0').css('margin-left','0').css('z-index',stickyzindex).removeClass('original').hide();
+    if ((adminbar) && $('body').hasClass('admin-bar') && (viewport > 600)) {
+      adminBarHeight = $('#wpadminbar').height();
+    } else {
+      adminBarHeight = 0;
     }
 
-    $('.cloned').css('left',leftOrgElement+'px').css('top',stickyTop+'px').css('width',widthOrgElement).show();
-    $('.original').css('visibility','hidden');
-  } else {
-    // not scrolled past the menu; only show the original menu.
-    $('.cloned').remove();
-    $('.original').css('visibility','visible');
+    if (($(window).scrollTop() >= (orgElementTop - stickyTop - adminBarHeight)) && (viewport >= minwidth) && (viewport <= maxwidth)) {
+
+      // scrolled past the original position; now only show the cloned, sticky element.
+
+      // Cloned element should always have same left position and width as original element.     
+      orgElement = $('.original');
+      coordsOrgElement = orgElement.offset();
+      leftOrgElement = coordsOrgElement.left;  
+      widthOrgElement = orgElement.css('width');
+      heightOrgElement = orgElement.outerHeight();
+      
+      // If padding is percentages, convert to pixels
+      paddingOrgElement = [orgElement.css('padding-top'), orgElement.css('padding-right'), orgElement.css('padding-bottom'), orgElement.css('padding-left')];
+      paddingCloned = paddingOrgElement[0] + ' ' + paddingOrgElement[1] + ' ' + paddingOrgElement[2] + ' ' + paddingOrgElement[3];
+
+      if( (dynamic == true) && ($('.cloned').length < 1)     ) {
+        // DYNAMIC MODE: if there is no clone present, create it right now
+        createClone(stickyTop,stickyZindex);
+      }
+
+      if ($(window).scrollTop() > (pushElementTop-heightOrgElement-adminBarHeight)) {
+        stickyTopMargin = (pushElementTop-heightOrgElement-$(window).scrollTop());
+      } else {
+        stickyTopMargin = adminBarHeight;
+      }
+
+      $('.cloned').css('left',leftOrgElement+'px').css('top',stickyTop+'px').css('width',widthOrgElement).css('margin-top',stickyTopMargin).css('padding',paddingCloned).show();
+      $('.original').css('visibility','hidden');
+    } else {
+      // not scrolled past the menu; only show the original menu.
+      if(dynamic == true) {
+        $('.cloned').remove();
+      } else {
+        $('.cloned').hide();
+      }
+      $('.original').css('visibility','visible');
+    }
   }
-}
+
+  function createClone(cloneTop,cloneZindex) {
+    $('.original').clone().insertAfter($('.original')).addClass('cloned').css('position','fixed').css('top',cloneTop+'px').css('margin-left','0').css('z-index',cloneZindex).removeClass('original').hide();
+  }
 
 }(jQuery));
