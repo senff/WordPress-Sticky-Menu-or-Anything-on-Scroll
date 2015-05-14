14ed9ddc47c2e1f38e30c55bf91bc99aaad74333
Mark Senff <mark@senff.com>
2015-05-14 09:17:08 -0400

    Update stickThis.js

--- assets/js/stickThis.js
+++ assets/js/stickThis.js
@@ -1,15 +1,20 @@
+/**
+* @preserve Sticky Anything 1.3 | @senff | GPL2 Licensed
+*/
+
 (function($) {
 	$(document).ready(function($) {
-
 
 		$(sticky_anything_engage.element).stickThis({
 			top:sticky_anything_engage.topspace,
 			minscreenwidth:sticky_anything_engage.minscreenwidth,
 			maxscreenwidth:sticky_anything_engage.maxscreenwidth,
 			zindex:sticky_anything_engage.zindex,
-			debugmode:sticky_anything_engage.debugmode
+			dynamicmode:sticky_anything_engage.dynamicmode,
+			debugmode:sticky_anything_engage.debugmode,
+			pushup:sticky_anything_engage.pushup,
+			adminbar:sticky_anything_engage.adminbar
 		});
 
 	});
 }(jQuery));
-
